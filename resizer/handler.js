5445b2cded7cab9a9730a401c015b4f3fbf530d1
Parag Gupta <sevenaces@gmail.com>
2020-07-28 22:39:46 +0530

    Temporarily reverted to original code from hackernoon

--- resizer/handler.js
+++ resizer/handler.js
@@ -1,101 +1,57 @@
-'use strict';
+"use strict";
 
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 const S3 = new AWS.S3({
-    signatureVersion: 'v4',
+  signatureVersion: "v4"
 });
-const Sharp = require('sharp');
+const Sharp = require("sharp");
 
 const BUCKET = process.env.BUCKET;
 const URL = process.env.URL;
-const ALLOWED_RESOLUTIONS = process.env.ALLOWED_RESOLUTIONS ? new Set(process.env.ALLOWED_RESOLUTIONS.split(/\s*,\s*/)) : new Set([]);
-const ALLOWED_PARAMS = ['webp']
+const ALLOWED_RESOLUTIONS = process.env.ALLOWED_RESOLUTIONS
+  ? new Set(process.env.ALLOWED_RESOLUTIONS.split(/\s*,\s*/))
+  : new Set([]);
 
-module.exports.resizer = function (event, context, callback) {
-    const key = event.queryStringParameters.key;
-    const match = key.match(/\d+x\d+,.*\/.*/)
-    let params = match[0].split(',');
-    let t = params[params.length-1].split('/')
-    params[params.length-1] = t[0]
-    params[params.length] = t[1]
-    let originalKey = '';
-    let args = [];
-    let width = 0;
-    let height = 0;
+module.exports.resizer = function(event, context, callback) {
+  const key = event.queryStringParameters.key;
+  const match = key.match(/((\d+)x(\d+))\/(.*)/);
 
-    let log = "";
+  //Check if requested resolution is allowed
+  if (0 != ALLOWED_RESOLUTIONS.size && !ALLOWED_RESOLUTIONS.has(match[1])) {
+    callback(null, {
+      statusCode: "403",
+      headers: {},
+      body: ""
+    });
+    return;
+  }
 
-    originalKey = params[params.length - 1];
+  const width = parseInt(match[2], 10);
+  const height = parseInt(match[3], 10);
+  const originalKey = match[4];
 
-    log += "Original Key: " + originalKey + "\n";
-
-    width = parseInt(params[0].split('x')[0]);
-    height = parseInt(params[0].split('x')[1]);
-
-    log += "Width: " + width + "\n";
-    log += "Height: " + height + "\n";
-
-
-    for (let i = 1; i < params.length; i++) {
-        if (ALLOWED_PARAMS.includes(params[i])) {
-            args.push(params[i]);
-        }
-    }
-
-    for (let j = 0; j < args.length; j++) {
-        log += "args: " + args[j] + "\n";
-    }
-
-
-    if (args.includes('webp'))
-    {
-        S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
-            .then(data => Sharp(data.Body)
-                .resize({width: width, height: height})
-                .toFormat('webp')
-                .webp({lossless: true})
-                .toBuffer()
-            )
-            .then(buffer => S3.putObject({
-                    Body: buffer,
-                    Bucket: BUCKET,
-                    ContentType: 'image/webp',
-                    Key: key,
-                }).promise()
-            )
-            .then(() => callback(null, {
-                    statusCode: '301',
-                    headers: {'location': `${URL}/${key}`},
-                    body: '',
-                })
-            )
-            .catch(err => callback(err, {
-                statusCode: '404',
-                body: err
-            }))
-    } else {
-        S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
-            .then(data => Sharp(data.Body)
-                .resize({width: width, height: height})
-                .toFormat('png')
-                .toBuffer()
-            )
-            .then(buffer => S3.putObject({
-                    Body: buffer,
-                    Bucket: BUCKET,
-                    ContentType: 'image/png',
-                    Key: key,
-                }).promise()
-            )
-            .then(() => callback(null, {
-                    statusCode: '301',
-                    headers: {'location': `${URL}/${key}`},
-                    body: '',
-                })
-            )
-            .catch(err => callback(err, {
-                statusCode: '404',
-                body: err
-            }));
-    }
-}
+  S3.getObject({ Bucket: BUCKET, Key: originalKey })
+    .promise()
+    .then(data =>
+      Sharp(data.Body)
+        .resize(width, height)
+        .toFormat("png")
+        .toBuffer()
+    )
+    .then(buffer =>
+      S3.putObject({
+        Body: buffer,
+        Bucket: BUCKET,
+        ContentType: "image/png",
+        Key: key
+      }).promise()
+    )
+    .then(() =>
+      callback(null, {
+        statusCode: "301",
+        headers: { location: `${URL}/${key}` },
+        body: ""
+      })
+    )
+    .catch(err => callback(err));
+};
