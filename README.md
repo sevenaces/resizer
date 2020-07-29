1aa6507132809f2a58d992ed2382f0df3f8cd350
Parag Gupta <sevenaces@gmail.com>
2020-07-29 11:35:32 +0530

    regex update

--- README.md
+++ README.md
@@ -104,7 +104,8 @@ Don't forget to replace `BUCKET_NAME` with your `BUCKET_NAME`
 
 
 ## Credits
-This is based on [this hackernoon post](https://hackernoon.com/image-resizing-after-upload-with-amazon-s3-aws-lambda-and-cloudfront-for-ssl-loce3y0h) with some changes.
+1. To [@pavangowdasu](https://github.com/pavangowdasu) with a lot of JS code! And in general, helping me with code always! 
+2. This is based on [this hackernoon post](https://hackernoon.com/image-resizing-after-upload-with-amazon-s3-aws-lambda-and-cloudfront-for-ssl-loce3y0h) with some changes.
 
 ## How to contribute
 Feel free to fork the repo and submit a pull request. I'm fairly novice at Javascript so I'm pretty sure someone else can seriously improve this.
--- resizer/handler.js
+++ resizer/handler.js
@@ -2,58 +2,68 @@
 
 const AWS = require("aws-sdk");
 const S3 = new AWS.S3({
-  signatureVersion: "v4"
+    signatureVersion: "v4"
 });
 const Sharp = require("sharp");
 
 const BUCKET = process.env.BUCKET;
 const URL = process.env.URL;
 const ALLOWED_RESOLUTIONS = process.env.ALLOWED_RESOLUTIONS
-  ? new Set(process.env.ALLOWED_RESOLUTIONS.split(/\s*,\s*/))
-  : new Set([]);
+    ? new Set(process.env.ALLOWED_RESOLUTIONS.split(/\s*,\s*/))
+    : new Set([]);
 const maxAge = 14 * 24 * 60 * 60
 
-module.exports.resizer = function(event, context, callback) {
-  const key = event.queryStringParameters.key;
-  const match = key.match(/((\d+)x(\d+))\/(.*)/);
+module.exports.resizer = function (event, context, callback) {
+    const key = event.queryStringParameters.key;
+    const match = key.match(/(?<webpsupport>webp)?\/?(?<dimensions>(?<height>\d+)x(?<width>\d+))\/(?<originalKey>.*)/);
 
-  //Check if requested resolution is allowed
-  if (0 != ALLOWED_RESOLUTIONS.size && !ALLOWED_RESOLUTIONS.has(match[1])) {
-    callback(null, {
-      statusCode: "403",
-      headers: {},
-      body: ""
-    });
-    return;
-  }
+    // Check if requested resolution is allowed
+    if (0 != ALLOWED_RESOLUTIONS.size && !ALLOWED_RESOLUTIONS.has(match.groups.dimensions)) {
+        callback(null, {
+            statusCode: "403",
+            headers: {},
+            body: ""
+        });
+        return;
+    }
 
-  const width = parseInt(match[2], 10);
-  const height = parseInt(match[3], 10);
-  const originalKey = match[4];
+    const width = parseInt(match.groups.width, 10);
+    const height = parseInt(match.groups.height, 10);
+    const originalKey = match.groups.originalKey;
 
-  S3.getObject({ Bucket: BUCKET, Key: originalKey })
-    .promise()
-    .then(data =>
-      Sharp(data.Body)
-        .resize(width, height)
-        .toFormat("png")
-        .toBuffer()
-    )
-    .then(buffer =>
-      S3.putObject({
-        Body: buffer,
-        Bucket: BUCKET,
-        ContentType: "image/png",
-        CacheControl: `max-age=${maxAge}`,
-        Key: key
-      }).promise()
-    )
-    .then(() =>
-      callback(null, {
-        statusCode: "301",
-        headers: { location: `${URL}/${key}` },
-        body: ""
-      })
-    )
-    .catch(err => callback(err));
+    let webpSupport = match.groups.webpsupport;
+
+    S3.getObject({Bucket: BUCKET, Key: originalKey})
+        .promise()
+        .then(function (data) {
+            let image = Sharp(data.Body).resize(width, height)
+
+            if (webpSupport) {
+                image = image.toFormat("webp")
+            } else {
+                image = image.toFormat("png")
+            }
+            return image.toBuffer()
+        })
+        .then(function (buffer) {
+            let contentType = 'image/png'
+            if (webpSupport) {
+                contentType = 'image/webp'
+            }
+             S3.putObject({
+                    Body: buffer,
+                    Bucket: BUCKET,
+                    ContentType: contentType,
+                    CacheControl: `max-age=${maxAge}`,
+                    Key: key
+                }).promise()
+        })
+        .then(() =>
+            callback(null, {
+                statusCode: "301",
+                headers: {location: `${URL}/${key}`},
+                body: ""
+            })
+        )
+        .catch(err => callback(err));
 };
